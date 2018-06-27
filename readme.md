2ddde8c05162a4ce70f632a5b1005f9d902e3549
Eric Gish <ericgish@Erics-iMac.local>
2018-06-27 08:24:16 -0600

    Added Files

---
+++ readme.md
@@ -0,0 +1,1 @@
+This package will swap single quotes to double, and double quotes to single within a selection. 
---
+++ spec/swapquotes-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import SwapquotesView from '../lib/swapquotes-view';
+
+describe('SwapquotesView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
